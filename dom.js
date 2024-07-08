f281b7e7742ea4340cb851bf0b6241c080689710
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-07-09 03:22:26 +0600

    js v2

--- dom.js
+++ dom.js
@@ -1,19 +1,30 @@
 document.getElementById('apply-bg').addEventListener('click', function(){
-    document.body.style.backgroundColor = 'tomato';
-
+    const friends = document.getElementsByClassName('friend');
+    for(const friend of friends){
+        friend.style.background = 'tomato';
+    }
 })
 
 document.getElementById('center-3rd').addEventListener('click', function(){
-    document.getElementsByClassName('friend')[2].style.textAlign = 'center';
+    const third = document.getElementsByClassName('friend')[2];
+    third.style.textAlign = 'center';
 })
-
 document.getElementById('add-friend').addEventListener('click', function(){
-    const friendContainer = document.getElementById('add-friend');
-    const newFriend = document.createElement('div');
-    newFriend.innerHTML = ` 
-<div class="friend">
-    <h3 class="friend-name">Friend-6</h3>
-    <p>Lorem ipsum dolor sit amet.</p>
-   </div> `
-    friendContainer.appendChild(newFriend);    
+    const friends = document.getElementsByClassName('friend').length;
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    const name = document.createElement('h3');
+    name.classList.add('friend-name');
+    name.innerText = `Friend-${friends + 1}`;
+    const content = document.createElement('p');
+    content.innerText = 'Lorem ipsum dolor sit amet.';
+    friend.appendChild(name);
+    friend.appendChild(content);
+    document.body.appendChild(friend);
+})
+document.getElementById('clear-all').addEventListener('click', function(){
+    const friends = document.getElementsByClassName('friend');
+    while(friends.length > 0){
+        friends[0].remove();
+    }
 })
