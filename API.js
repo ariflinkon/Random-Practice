210064585f50700fa26727e78b6035398c9477eb
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-07-16 21:07:13 +0600

    API

---
+++ API.js
@@ -0,0 +1,11 @@
+import { HfInference } from "@huggingface/inference";
+
+const inference = new HfInference("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");
+
+for await (const chunk of inference.chatCompletionStream({
+	model: "meta-llama/Meta-Llama-3-8B-Instruct",
+	messages: [{ role: "user", content: "What is the capital of France?" }],
+	max_tokens: 500,
+})) {
+	process.stdout.write(chunk.choices[0]?.delta?.content || "");
+}
---
+++ Array-Map-To-Do-One-Line-Loop-Magic.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// Using map to create a new array where each element is doubled
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
